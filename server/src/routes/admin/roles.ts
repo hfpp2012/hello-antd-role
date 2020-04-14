02f30bad124cfc3316eb1ef263d45e647529ca94
hfpp2012 <hfpp2012@gmail.com>
2020-04-14 23:15:56 +0800

    30_permission

--- server/src/routes/admin/roles.ts
+++ server/src/routes/admin/roles.ts
@@ -1,11 +1,12 @@
 import express, { Router } from "express";
 import * as rolesController from "../../controllers/admin/roles";
 import checkAdminAuthMiddleware from "../../middlewares/admin/check-auth.middleware";
+import allow from "../../middlewares/admin/roles.middleware";
 
 const router: Router = express.Router();
 
 router
-  .get("/", checkAdminAuthMiddleware, rolesController.index)
+  .get("/", checkAdminAuthMiddleware, allow("read role"), rolesController.index)
   .post("/", checkAdminAuthMiddleware, rolesController.addRole);
 
 router.put("/:id", checkAdminAuthMiddleware, rolesController.updateRole);
--- server/src/routes/admin/users.ts
+++ server/src/routes/admin/users.ts
@@ -1,13 +1,19 @@
 import express, { Router } from "express";
 import * as usersController from "../../controllers/admin/users";
 import checkAdminAuthMiddleware from "../../middlewares/admin/check-auth.middleware";
+import allow from "../../middlewares/admin/roles.middleware";
 
 const router: Router = express.Router();
 
 router.post("/login", usersController.postLogin);
 
 router
-  .get("/", checkAdminAuthMiddleware, usersController.index)
+  .get(
+    "/",
+    checkAdminAuthMiddleware,
+    allow("read admin"),
+    usersController.index
+  )
   .post("/", checkAdminAuthMiddleware, usersController.addAdmin)
   .get("/currentUser", checkAdminAuthMiddleware, usersController.currentUser);
 
